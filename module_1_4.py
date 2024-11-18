038800a9e67551f93cd04fddb25f197576dc0cf5
Mellianna <anetelnaraiya@gmail.com>
2024-11-18 20:07:30 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input(' Эта строка с произвольным текстом, введите что-нибудь (с пробелами): ')
+print('В введёном вами тексте (включая пробелы): ', len(my_string), 'символов')
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
