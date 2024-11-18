dc69fdba7576cacb687c957898a36d8b660a9e7a
Mellianna <anetelnaraiya@gmail.com>
2024-11-18 13:28:58 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = 'Anna'
+print('Name:' + name)
+age = 27
+print('Age:' + (str(age)))
+new_age = (age + 1)
+print('New age:' + str(new_age))
+is_student = True
+print('Is Student:' + str(is_student))
