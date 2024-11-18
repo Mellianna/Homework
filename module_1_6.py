be384d3fff02e73ca17ef921a6864cd1735ba6f4
Mellianna <anetelnaraiya@gmail.com>
2024-11-18 14:37:49 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,7 @@
+namber_of_completed_tasks = 12
+namber_of_hours_spent = 1.5
+cours_title = 'Python'
+comma = ','
+stp = '.'
+tame_for_one_task = (namber_of_hours_spent / namber_of_completed_tasks)
+print(cours_title , comma ,  'всего задач:' ,  namber_of_completed_tasks , comma , 'затрачено часов:' ,  namber_of_hours_spent , comma , 'среднее время выполнения' , tame_for_one_task , stp)
