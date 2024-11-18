9c1af0030a2c37a4fdfadcb9c50cdc181f47a098
Mellianna <anetelnaraiya@gmail.com>
2024-11-18 18:58:20 +0300

    Update module_1_6.py

--- module_1_6.py
+++ module_1_6.py
@@ -4,4 +4,4 @@ cours_title = 'Python'
 comma = ','
 stp = '.'
 tame_for_one_task = (namber_of_hours_spent / namber_of_completed_tasks)
-print(cours_title , comma ,  'всего задач:' ,  namber_of_completed_tasks , comma , 'затрачено часов:' ,  namber_of_hours_spent , comma , 'среднее время выполнения' , tame_for_one_task , stp)
+print(cours_title , comma ,  'всего задач:' ,  namber_of_completed_tasks , comma , 'затрачено часов:' ,  namber_of_hours_spent , comma , 'среднее время выполнения' , tame_for_one_task , 'часа' , stp)
