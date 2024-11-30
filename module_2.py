5ae790c4dbb3230ec33b364127ed9c99293e1d84
Mellianna <anetelnaraiya@gmail.com>
2024-11-30 20:17:01 +0300

    Update module_2.py

--- module_2.py
+++ module_2.py
@@ -2,8 +2,8 @@ import random
 first_numder = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 n = random.choice(first_numder)
 result = []
-for i in range(1, 19):
-    for j in range(i+1, 19):
+for i in range(1, 20):
+    for j in range(i+1, 20):
         if n % (i+j) == 0:
             result.append(i)
             result.append(j)
