11246dadd8d93dc963102767af41cbbf50bb93e4
Mellianna <anetelnaraiya@gmail.com>
2024-12-23 16:00:40 +0300

    Add files via upload

---
+++ module_3.py
@@ -0,0 +1,25 @@
+def calculate(data_structure):
+    summa = 0
+    if isinstance(data_structure, (list, tuple, set)):
+        for item in data_structure:
+            summa += calculate(item)
+    if isinstance(data_structure, (int, float)):
+        summa += data_structure
+    if isinstance(data_structure, str):
+        summa += len(data_structure)
+    if isinstance(data_structure, dict):
+        for key, value in data_structure.items():
+            summa += calculate(key)
+            summa += calculate(value)
+    return summa
+
+data_structure = [
+[1, 2, 3],
+{'a': 4, 'b': 5},
+(6, {'cube': 7, 'drum': 8}),
+"Hello",
+((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+result = calculate(data_structure)
+print(result)
