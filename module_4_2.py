73d2bd64cac27417a94be72feced013d0f2a6e28
Mellianna <anetelnaraiya@gmail.com>
2024-12-24 19:19:02 +0300

    Update module_4_2.py

--- module_4_2.py
+++ module_4_2.py
@@ -4,3 +4,6 @@ def test_function():
     inner_function()
 
 test_function()
+
+# inner_function() - работать не будет, тк эта функция существует только во время работы test_function и находится в ее локальном пространстве
+
